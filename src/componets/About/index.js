796bdb2f3cdda02fda9a865fb3c03ef8c4613756
Ryebread5555 <rypetty55@gmail.com>
2023-04-22 11:04:11 -0500

    updated about me page

---
+++ src/componets/About/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+
+function About() {
+    return(
+        <section className='title'>
+
+            <div className='row justify-content-center' id='about-container'>
+            <Col lg={6} md={12}>
+            <p class='name'>Ryan Petty</p>
+            <p class='job'>Junior Full Stack Web Developer</p>
+            <h2 class='education'>My school experience</h2>
+
+            <p>
+
+            
+            <br></br>
+            <br></br>
+
+            <br></br>
+            <br></br>
+
+
+            </p>    
+            </Col>
+            </div>
+        </section>
+    )
+};
+
+export default About;
