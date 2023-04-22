82c61dfd5a6490eb7adc935633e8abf68c5c7c2e
Ryebread5555 <rypetty55@gmail.com>
2023-04-22 11:05:06 -0500

    added header navigation

---
+++ src/componets/Header/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Navigation from '../Navigation';
+import Project from '../Project';
+import Contact from '../Contact';
+import About from '../About';
+
+function Portfolio () {
+    // this will use the state of the current page
+    const [currentPage, handlePageChange] = useState('About');
+
+const renderPage = () => {
+    // a switch statement that returns the componet of currentPage
+    switch(currentPage) {
+        case 'about':
+        return <About></About>
+        case 'contact':
+        return <Contact></Contact>
+        case 'portfolio':
+        return <Project></Project>
+        default:
+            return <About></About>
+    }
+};
+
+return (
+    <div>
+        <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+    <div>
+    {
+        renderPage()
+    }
+    </div>
+    </div>
+);
+
+}
+
+export default Portfolio;
