8a9a0116d59e5a04331da6882baa798db6e26b61
Ryebread5555 <Rypetty55@gmail.com>
2023-04-23 15:13:03 -0500

    added placeholder text

--- src/componets/About/index.js
+++ src/componets/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Col from 'react-bootstrap/Col';
+import { Col } from 'react-bootstrap';
 
 function About() {
     return(
@@ -12,15 +12,12 @@ function About() {
             <h2 class='education'>My school experience</h2>
 
             <p>
-
-            
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             <br></br>
             <br></br>
-
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             <br></br>
             <br></br>
-
-
             </p>    
             </Col>
             </div>
