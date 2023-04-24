af463eba134961e5b22206070a7d9d810d482ece
Ryebread5555 <Rypetty55@gmail.com>
2023-04-23 21:07:19 -0500

    added profile image

--- src/componets/About/index.js
+++ src/componets/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import profilePic from '../../assets/Profile-pic.jpg'
 
 function About() {
     return(
@@ -7,9 +8,10 @@ function About() {
 
             <div className='row justify-content-center' id='about-container'>
             <Col lg={6} md={12}>
-            <p class='name'>Ryan Petty</p>
-            <p class='job'>Junior Full Stack Web Developer</p>
-            <h2 class='education'>My school experience</h2>
+            <center><img src={profilePic} className='profile-pic' alt='my-profile'/></center>
+            <p className='name'>Ryan Petty</p>
+            <p className='job'>Junior Full Stack Web Developer</p>
+            <h2 className='education'>About me</h2>
 
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
