f69666cc4dd88db76f4c5101918549fc4a44dc2d
Ryebread5555 <Rypetty55@gmail.com>
2023-04-23 21:35:56 -0500

    updated to include resume and look cleaner

--- src/componets/Resume/index.js
+++ src/componets/Resume/index.js
@@ -1,77 +1,62 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { BsFillFileEarmarkWordFill } from 'react-icons/bs';
+import Resume from '../../assets/resume-image.jpg'
 
-function ResumeForm () {
+function ResumeForm() {
+  return (
+    <section>
+      <div className='resume-container'>
+        <Row>
+          <Col lg={4} md={12}>
+            <h2 className='resume'>Front-end proficiencies</h2>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>React.js</li>
+              <li>Frameworks: Bootstrap</li>
+            </ul>
+          </Col>
 
-    return (
-        <section>
+          <Col lg={4} md={12}>
+            <h2 className='resume'>Back-end proficiencies</h2>
+            <ul>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>MySQL: mysql12</li>
+              <li>NoSQL: MongoDB, Mongoose</li>
+              <li>API's: RESTful, server-side</li>
+              <li>Template language: Handlebars</li>
+            </ul>
+          </Col>
 
-            <div className='resume-container'>
-                <Row>
-                    <Col lg={4} md={12}>
-                        <h2 className='resume'>Front-end proficiencies</h2>
-                        <ul>
-                            <li>
-                                HTML
-                            </li>
-                            <li>
-                                CSS
-                            </li>
-                            <li>
-                                JavaScript
-                            </li>
-                            <li>
-                                jQuery
-                            </li>
-                            <li>
-                                React.js
-                            </li>
-                            <li>
-                                Frameworks: Bootstrap
-                            </li>
-                        </ul>
-                    </Col>
-
-                    <Col lg={4} md={12}>
-                        <h2 className='resume'>Back-end proficiencies</h2>
-                        <ul>
-                            <li>
-                                Node.js
-                            </li>
-                            <li>
-                                Express.js
-                            </li>
-                            <li>
-                                MySQL: mysql12
-                            </li>
-                            <li>
-                                NoSQL: MongoDB, Mongoose
-                            </li>
-                            <li>
-                                API's: RESTful, server-side
-                            </li>
-                            <li>
-                                Template language: Handlebars
-                            </li>
-                        </ul>
-                    </Col>
-
-                    <Col lg={4} md={12}>
-                        <h2 className='education'>Education</h2>
-                        <p>
-                            <span className='school-name'>Northwestern University</span><br></br>
-                            Full Stack Web Development Bootcamp
-                            <br></br>
-                            <br></br>
-                            <span className='school-name'>Harper College</span><br></br>
-                            Associate  in Arts<br></br>
-                            E-Marketing<br></br>
-                        </p>
-                    </Col>
-                </Row>
-            </div>
-        </section>
-    );
+          <Col lg={4} md={12}>
+            <h2 className='education'>Education</h2>
+            <p>
+              <span className='school-name'>Northwestern University</span>
+              <br></br>
+              Full Stack Web Development Bootcamp
+              <br></br>
+              <br></br>
+              <span className='school-name'>Harper College</span>
+              <br></br>
+              Associate in Arts
+              <br></br>
+              E-Marketing
+            </p>
+            <Col lg={3} md={12}>
+              <a href={Resume} download>
+                <BsFillFileEarmarkWordFill />
+              </a>
+              <p className='icon-text'>Resume</p>
+            </Col>
+          </Col>
+        </Row>
+      </div>
+    </section>
+  );
 }
 
 export default ResumeForm;
