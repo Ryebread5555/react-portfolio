dd4e03fbb119662854b3c51423cf48c810c03ecd
Ryebread5555 <Rypetty55@gmail.com>
2023-04-23 21:08:15 -0500

    fixed navigation with switch statement!

--- src/componets/Header/index.js
+++ src/componets/Header/index.js
@@ -9,21 +9,22 @@ function Portfolio () {
     // this will use the state of the current page
     const [currentPage, handlePageChange] = useState('About');
 
-const renderPage = () => {
-    // a switch statement that returns the componet of currentPage
-    switch(currentPage) {
-        case 'about':
-        return <About></About>
-        case 'contact':
-        return <Contact></Contact>
-        case 'portfolio':
-        return <Project></Project>
-        case 'resume':
-        return <Resume></Resume>
-        default:
-            return <About></About>
-    }
-};
+    const renderPage = () => {
+        // a switch statement that returns the componet of currentPage
+        switch(currentPage.toLowerCase()) {
+          case 'about':
+            return <About />;
+          case 'contact':
+            return <Contact />;
+          case 'portfolio':
+            return <Project />;
+          case 'resume':
+            return <Resume />;
+          default:
+            return <About />;
+        }
+      };
+      
 
 return (
     <div>
