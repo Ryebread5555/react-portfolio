b931da565cf567d0b5a0af02d33f259e561587ee
Ryebread5555 <Rypetty55@gmail.com>
2023-04-23 21:09:46 -0500

    updated github to direct to correct repo

--- src/componets/Project/index.js
+++ src/componets/Project/index.js
@@ -3,32 +3,37 @@ import projects from '../../projects.json';
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCard(props) {
-    return (
-        <div className='card'>
-            <div className='img-container'>
-                <img alt={props.name} src={props.image}/>
+  const handleIconClick = () => {
+    window.open(props.github, '_blank');
+  };
+
+  return (
+    <div className='card'>
+      <div className='img-container'>
+        <img alt={props.name} src={props.image}/>
+      </div>
+      <div className='content'>
+        <ul>
+          <li>
+            <p className='project-title'>{props.name}</p>
+          </li>
+          <li>
+            <p className='project-description'>{props.description}</p>
+          </li>
+          <li>
+            <div className='project-icons'>
+              <BsGithub onClick={handleIconClick} />
             </div>
-            <div className='content'>
-                <ul>
-                    <li>
-                        <p className='project-title'>{props.name}</p>
-                    </li>
-                    <li>
-                        <p className='project-description'>{props.description}</p>
-                    </li>
-                    <li>
-                        <div className='project-icons'>
-                        <a href='https://github.com/Ryebread5555'><BsGithub /></a>
-                        </div>
-                    </li>
-                    <li>
-                        {props.skills}
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+          </li>
+          <li>
+            {props.skills}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
+
 
 function Wrapper(props) {
     return <div className='wrapper'>{props.children}</div>;
