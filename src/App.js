707174c2b9cdc21f38852d167f6dfa4d46015598
Ryebread5555 <rypetty55@gmail.com>
2023-04-24 13:25:11 -0500

    adding everything back to main

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './componets/Header';
+import Footer from './componets/Footer';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <Header></Header>
+    </div>
+    <div>
+      <Footer></Footer>
+    </div> 
+    </>
+  );
+}
+
+export default App;
---
+++ src/componets/Contact/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helper';
+
+function ContactForm () {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+          const isValid = validateEmail(e.target.value);
+      
+          if (!isValid) {
+            setErrorMessage('Your email is invalid');
+          } else {
+            setErrorMessage('');
+          }
+        } else {
+          if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+          } else {
+            setErrorMessage('');
+          }
+        }
+        if (!errorMessage) {
+          setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+      }
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+return (
+         <section>
+         <h1 data-testid='h1tag' className="contact">CONTACT ME</h1>
+         <form id="contact-form">
+            <div class="col-12">
+                <label htmlFor="name">Name:</label>
+                <input type="text" class="form-control" width="100px" defaultValue={name} onBlur={handleChange} name="name" />
+             </div>
+             <div class="col-12">
+                <label htmlFor="email">Email address:</label>
+                <input type="email" class="form-control" defaultValue={email} onBlur={handleChange} name="email" />
+            </div>
+             <div class="col-12">
+                <label htmlFor="message">Message:</label>
+                <textarea name="message" class="form-control" defaultValue={message} onBlur={handleChange} rows="7" />
+            </div> 
+            {errorMessage && (
+            <div>
+                <p className="error-text">{errorMessage}</p>
+            </div>
+            )}
+            <br></br>
+            <div class="col-12">
+            <button data-testid='button' class="btn btn-outline-dark" type="submit" onSubmit={handleSubmit}>Submit</button>
+            </div>
+        </form>
+    </section> 
+)
+
+}
+
+export default ContactForm;
+
+
