f863f303b863d9389f6e975c97b0df277044a268
Ryebread5555 <Rypetty55@gmail.com>
2023-05-21 18:30:43 -0500

    fixed spelling

--- src/componets/About/index.js
+++ src/componets/About/index.js
@@ -14,7 +14,7 @@ function About() {
             <h2 className='education'>About me</h2>
 
             <p>
-            Hello, my name is Ryan. I recently switched my education to web development and studyied rigorously at Northwestern University. I wanted to challenge myself in new ways by exploring the world of coding. Web Development allows me to be creative and find ways to optimize certain processes. Everything and anything can always be improved and I enjoy being able to solve issues that challenge my mind!
+            Hello, my name is Ryan. I recently switched my education to web development and studied rigorously at Northwestern University. I wanted to challenge myself in new ways by exploring the world of coding. Web Development allows me to be creative and find ways to optimize certain processes. Everything and anything can always be improved and I enjoy being able to solve issues that challenge my mind!
             <br></br>
             <br></br>
             I am a full-stack Web Developer based in the Chicago-land area with certificates in e-Marketing and Web Development. I studied at Harper college to obtain my Associate in Arts and spent a lot of my time working hard to achieve my educational goals. I am a hands on person who likes to take things apart and put them back together to understand how things work. Some of my hobbies include: Building PC's, Studying game design, and, Video production/editing. I am a hard worker who is always putting my all into everything. I enjoy learning and am looking forward to learning more during my coding journey!
